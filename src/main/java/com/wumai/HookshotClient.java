2ee0a5ceca3ec546445ce64541c44dfe3957e3cf
REAL <7390279+trick12138@user.noreply.gitee.com>
2024-01-03 16:46:26 +0800

    变更

---
+++ src/main/java/com/wumai/HookshotClient.java
@@ -0,0 +1,10 @@
+package com.wumai;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HookshotClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/wumai/HookshotDataGenerator.java
@@ -0,0 +1,11 @@
+package com.wumai;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HookshotDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
